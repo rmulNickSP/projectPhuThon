c51d47ec3af2982acfe38ddf58342e5d236d9eb0
rmulNickSP <51589535+rmulNickSP@users.noreply.github.com>
2019-06-13 15:35:28 +0700

    try

---
+++ add_user.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
+<body>
+<?php
+include("connect_db.php");
+$con = connect_db();
+$sql="INSERT INTO user (id,permiss_id,user_id,pass_id,card_id) 
+VALUES(' ','3','$_POST[usrname]','$_POST[psw]','$_POST[idcard]')";
+
+// $sql_user = mysqli_query($con,$sql)or die("add_user/ insert error".mysqli_error($con));
+
+mysqli_close($con);
+
+?>
+</body>
+</html>
+
+<script>
+
+window.location.href = "index.html";
+
+
+
+</script>
---
+++ js/validateSetdef.js
@@ -0,0 +1,18 @@
+jQuery.validator.setDefaults({
+		
+    highlight: function(element) {
+        jQuery(element).closest('.form-control').addClass('is-invalid');
+    },
+    unhighlight: function(element) {
+        jQuery(element).closest('.form-control').removeClass('is-invalid');
+    },
+    errorElement: 'span',
+    errorClass: 'valid-feedback d-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
