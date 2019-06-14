d9eb504a75585887dcfd982b9f1d9f9213887a89
rmulNickSP <51589535+rmulNickSP@users.noreply.github.com>
2019-06-14 12:42:57 +0700

    14/6/2019 12:42

--- add_user.php
+++ add_user.php
@@ -11,25 +11,11 @@
 include("connect_db.php");
 $con = connect_db();
 
-echo $_POST['usrname'];
-echo "<hr>";
+$sql = "INSERT INTO user (id, permiss_id, user_id, pass_id,card_id)
+VALUES (' ', '3', '$_POST[usrname]', '$_POST[psw]', '$_POST[idcard]')";
 
-$select_sql = mysqli_query($con,"SELECT user_id FROM user
-WHERE user_id LIKE '$_POST[usrname]%';")
+$sql_user = mysqli_query($con,"INSERT INTO user (id, permiss_id, user_id, pass_id,card_id) VALUES ('', '3', '$_POST[usrname]', '$_POST[psw]', '$_POST[idcard]')")or die("add_user/ insert error".mysqli_error($con));
 
-// $sql = 'INSERT INTO user (id, permiss_id, user_id, pass_id,card_id)
-// VALUES ('', '3', '$_POST[usrname]', '$_POST[psw]', '$_POST[idcard]')'
-
-if($select_sql==true){
-    <script>
-    swal({
-        title: "สมัครสมาชิกสำเร็จ",
-        icon: "error",
-        button: "ตกลง",
-    </script>
-}else{
-// $sql_user = mysqli_query($con,"INSERT INTO user (id, permiss_id, user_id, pass_id,card_id) VALUES ('', '3', '$_POST[usrname]', '$_POST[psw]', '$_POST[idcard]')")or die("add_user/ insert error".mysqli_error($con));
-}
 mysqli_close($con);
 
 ?>
